f6468481538e7d479256d38576e2ffb555484fc1
pankov <frostkslo1@yandex.ru>
2021-08-06 22:15:07 +0300

    Теперь можно будет показывать решение

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const nunjucks = require("nunjucks");
 const mathjs = require("mathjs")
 var linSolve = require("robust-linear-solve")
 
-const {sum, re} = require("mathjs");
+const {sum, re, round} = require("mathjs");
 const app = express();
 
 const urlencodedParser = bodyParser.urlencoded({extended: false});
@@ -58,6 +58,62 @@ function MH(legs) {
 
     }
     return dict_of_i
+}
+
+
+function* MH_out(legs) {
+    let dict_of_i = new Map();
+    for (let i = 0; i < legs.length; i++){
+        dict_of_i.set(i + 1, [])
+    }
+    for (let el = 0; el < legs.length; el++) {
+        let minus_or_plus;
+        if (legs[el][1] === 'R')
+            minus_or_plus = -1
+        else
+            minus_or_plus = 1
+        let r1 = 0;
+        let list_of_r1 = []
+        let list_of_r1_fuck = []
+        for (let i = 0; i < legs.length; i++){
+            if (i !== el){
+                r1 += (1 / legs[i][0]) ** -1
+                list_of_r1.push(`1/${legs[i][0]}`)
+                list_of_r1_fuck.push(legs[i][0])
+            }
+        }
+        r1 = mathjs.round(r1, 2)
+        let str_r1 = ''
+        if (list_of_r1.length > 2){
+            str_r1 += '('
+            str_r1 += join(list_of_r1, "+")
+            str_r1 += ') ^ -1'
+        } else if (list_of_r1.length){
+            str_r1 += `${list_of_r1_fuck[0]} * ${list_of_r1_fuck[1]}/(${list_of_r1_fuck[0]} + ${list_of_r1_fuck[1]})`
+        }
+        yield `${str_r1} = ${r1}`
+        let i = (legs[el][2] / (r1 + legs[el][0]))
+        i = mathjs.round(i, 2)
+        let arr = dict_of_i.get(el + 1)
+        arr.push(i * minus_or_plus)
+        dict_of_i.set(el + 1, arr)
+        yield `${r1} + ${legs[el][0]} = ${r1 + legs[el][0]}`
+        yield [el + 1, legs[el][2] + '/' + r1 + legs[el][0]]
+        yield i * minus_or_plus
+        let uab = mathjs.round(r1 * i, 2)
+        yield `${r1} * ${i} = ${uab}`
+        for (let i = 0; i < legs.length; i++){
+            if (i != el){
+                let arr = dict_of_i.get(el + 1)
+                arr.push(mathjs.round((uab / legs[i][0] * -1 * minus_or_plus), 2))
+                dict_of_i.set(el + 1, arr)
+                yield [i + 1, uab + '/' + legs[i][0]]
+                yield [mathjs.round(uab / legs[i][0] * -1 * minus_or_plus, 2)]
+            }
+        }
+
+    }
+    yield dict_of_i
 }
 
 function MYH(legs) {
@@ -159,6 +215,34 @@ app.post("/phys/task_1", urlencodedParser, function (request, response) {
     console.log(MYH(array))
     console.log(MH(array))
     console.log(MYKY(array))
+    let result = ''
+    let MH_gen = MH_out(array)
+    for (let i = 0; i < array.length; i++) {
+        result += i + 1 + '.' + '\n'
+        result += 'R1 = ' + MH_gen.next().value + '\n'
+        result += 'Rэ = ' + MH_gen.next().value + '\n'
+
+        result += 'I = ' + MH_gen.next().value.join(' = ') + ' = ' + MH_gen.next().value + '\n'
+        result += 'Uab = ' + MH_gen.next().value + '\n'
+        for (let g = 0; g < array.length - 1; g++) {
+            console.log(result)
+            result += 'I' + MH_gen.next().value.join(' = ') + ' = ' + MH_gen.next().value + '\n'
+        }
+    }
+    let dict_ = MH_gen.next().value
+    result += '\n\nTrue currents: \n'
+    for (let i = 1; i <= array.length; i++){
+        let sum_i = ''
+        let arr = dict_.get(i)
+        for (let g = 0; g < arr.length; g++){
+            if (g === 0)
+                sum_i += arr[g]
+            else
+                sum_i += arr[g] < 0 ? ' - ' : ' + ' + mathjs.abs(arr[g])
+        }
+        result += `I${i} = ${sum_i} = ${round(sum(arr), 2)}\n`
+    }
+    console.log(result)
     response.render("first_task_for_edit.html", {ran: ran, elems: json, lines:parseInt(request.body.lines)})
 })
 app.listen(3000, "localhost");
